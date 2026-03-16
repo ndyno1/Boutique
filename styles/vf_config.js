86d3a64f6a84962c175d90c8ea39e6664e8d886a
ndyno1 <otowekeandy@gmail.com>
2026-03-16 19:14:33 +0100

    Update vf_config.js

--- styles/vf_config.js
+++ styles/vf_config.js
@@ -1,1 +1,28 @@
-vf_config.js
+(function () {
+  const ref = document.referrer || "";
+  const inAppsScriptPanel = /script\.googleusercontent\.com|script\.google\.com/i.test(ref);
+
+  window.VF_CONFIG = {
+    brandName: "ViralFlowr",
+    companyName: "DI corporation",
+    canonHost: "viralflowr.com",
+
+    supportEmail: "viralflowr@gmail.com",
+    whatsappNumber: "243838694889",
+    whatsappUrl: "https://wa.me/243838694889",
+    telegramUrl: "https://t.me/Viralflow",
+
+    scriptUrl: "https://script.google.com/macros/s/AKfycbx_Tm3cGZs4oYdKmPieUU2SCjegAvn-BTufubyqZTFU4geAiRXN53YYE9XVGdq_uq1H/exec",
+    avisScriptUrl: "https://script.google.com/macros/s/AKfycbylffBPr9m8Y26asnlPuVDizwS5xOUe9zpUIt3NrbUZlVsMgNkh9bOcANYbkvTZbpL9/exec",
+    orderScriptUrl: "https://script.google.com/macros/s/AKfycbyCvuy-WiLMlAkBb7k6YyPVMk4lQhGGke05heSWSw--twKE2L-oVSOs884g3jn6lt6m/exec",
+
+    timeoutMs: 25000,
+    debug: false,
+    proxyUrl: "/vf_proxy",
+    cookieDomain: ".viralflowr.com",
+    cookieTokenKey: "vf_token",
+
+    inAppsScriptPanel: inAppsScriptPanel,
+    postMessageTargetOrigin: inAppsScriptPanel ? "*" : location.origin
+  };
+})();
